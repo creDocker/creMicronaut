c0a6cd29d8c002946d6eb63b719fab9943b8d02d
Michael Kahle <michael.kahle@yahoo.de>
2023-02-11 06:59:53 +0100

    initial version

---
+++ cre/micronaut-test.sh
@@ -0,0 +1,22 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+cat /cre/versions.txt
+
+##if [ ! -f /cre/node-procfile ]; then
+##    echo "[FAIL]: File /cre/node-procfile not found!"
+##    exit 1
+##fi
+
+isInFile=$(cat /cre/versions.txt | grep -c "npm")
+if [ $isInFile -eq 0 ]; then
+    echo "[FAIL]: npm not installed!"
+    exit 1
+fi
+
+echo "[SUCCESS]"
+exit 0
