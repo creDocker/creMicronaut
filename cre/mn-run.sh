d31b6e0c7da74e25eac723ced3798b687e03fc44
Michael Kahle <michael.kahle@yahoo.de>
2023-02-26 21:10:47 +0100

    maven run error - may due java jdk version etc

--- cre/mn-run.sh
+++ cre/mn-run.sh
@@ -4,6 +4,8 @@ sleep 10
 
 echo "try to run maven"
 
+# https://stackoverflow.com/questions/53759658/e-kapt-com-sun-tools-javac-util-context-class-cant-be-found-tools-jar-i
+
 cd /cre/mn-apps/mn-init
 ./mvnw mn:run
 
