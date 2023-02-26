c962f9001f599020fc9665c70206a833bff1ae92
Michael Kahle <michael.kahle@yahoo.de>
2023-02-26 20:30:58 +0100

    port 8080

--- cre/mn-init.sh
+++ cre/mn-init.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash 
 # sleep 10000
 
+# https://guides.micronaut.io/latest/creating-your-first-micronaut-app-maven-kotlin.html
+
 cd /cre/mn-apps
-/cre/micronaut.sh create-app mn-init --build maven 
+## --build=gradle|maven
+## --lang=java|kotlin
+
+/cre/micronaut.sh create-app mn-init --build maven --lang=kotlin
+##/cre/micronaut.sh create-app domain.mn-init  -> domain = namespace
+
 ls -l /cre/mn-apps/mn-init
 
