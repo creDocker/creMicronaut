79defa9f69531ab8c142be1260f65b6fc3155e19
Michael Kahle <michael.kahle@yahoo.de>
2023-03-02 22:04:24 +0100

    java

--- cre/mn-init.sh
+++ cre/mn-init.sh
@@ -2,6 +2,7 @@
 # sleep 10000
 
 # https://guides.micronaut.io/latest/creating-your-first-micronaut-app-maven-kotlin.html
+# https://micronaut-projects.github.io/micronaut-starter/latest/guide/#commands
 
 cd /cre/mn-apps
 ## --build=gradle|maven
@@ -9,6 +10,8 @@ cd /cre/mn-apps
 
 #for now cleanup dir first
 rm -Rf /cre/mn-apps/mn-init
+
+/cre/micronaut.sh create-app --list-features
 
 /cre/micronaut.sh create-app mn-init --build=maven --lang=kotlin
 ##/cre/micronaut.sh create-app domain.www.mn-init  -> domain.subdomain = namespace;  xx-yy => subdirs??
