be75deb61099181597a68b7899f87ac5960876b3
KMicha <michael.kahle@yahoo.de>
2024-06-17 17:42:49 +0200

    generate ppom.xml

--- cre/mn-run.sh
+++ cre/mn-run.sh
@@ -7,7 +7,8 @@ echo "try to run maven"
 # https://stackoverflow.com/questions/53759658/e-kapt-com-sun-tools-javac-util-context-class-cant-be-found-tools-jar-i
 
 cd /cre/mn-app
-./mvnw mn:run
+## ./mvnw mn:run
+/cre/mn-app/mvnw mn:run
 
 
 
